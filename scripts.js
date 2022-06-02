ed1a29f550c18291bb387dbf02a47fb017beb70c
Ethan Hearn <hearn@Ethans-MacBook-Pro.local>
2022-06-02 13:43:07 -0400

    Scripting finished

--- scripts.js
+++ scripts.js
@@ -1,15 +1,27 @@
-const winning_combinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8]
-]
-
+let circleTurn
+let turns = 0;
+let defaultDomState = document.body.innerHTML
 const game = (() => {
 
+
+    const winning_combinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [6, 4, 2]
+    ]
+    
+
+    const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+    const endGameScreen = document.getElementById("winning-message")
+    const restart = document.getElementById("restartButton");
+
+
+    // Check all win conditions
     const checkWin = (currentClass) => {
         return winning_combinations.some(combination => {
             return combination.every(index => {
@@ -18,32 +30,57 @@ const game = (() => {
         })
     }
 
+    const checkTurns = () => {
+        return turns;
+    }
+
+    const isDraw = () => {
+        return checkTurns()>=8;
+    }
+
     // Place player mark
     const placeMark = (cell, currentClass) => {
         cell.classList.add(currentClass);
+        console.log (circleTurn);
     }
 
+    const endGame = (draw) => {
+        if (draw) {
+            winningMessageTextElement.innerText = "It's a draw!";
+        } else {
+            winningMessageTextElement.innerText = `${circleTurn ? "O" : "X"} Wins!`;
+        }
+        endGameScreen.classList.add("show");
+
+    }
+
+    const reset = () => {
+        endGameScreen.classList.remove("show");
+        board.startGame();
+    }
+
+    restart.addEventListener('click', reset)
+
     return {
+        turns,
         checkWin,
-        placeMark
+        checkTurns,
+        isDraw,
+        placeMark,
+        endGame,
     }
 })()
+
 const board = (() => {
 
     const X_CLASS = 'x';
     const CIRCLE_CLASS = 'circle';
-    let circleTurn;
 
     // Select gameboard on DOM
     const gameBoard = document.getElementById('board')
 
     // Select DOM cells
     const cellElements = document.querySelectorAll('[data-cell]');
-    
-    // Event Listener to handle clicks
-    cellElements.forEach(cell => {
-        cell.addEventListener('click', handleClick, {once: true})
-    });
     
     function handleClick(e) {
         // Place Mark
@@ -53,12 +90,39 @@ const board = (() => {
 
         // Check For Win
         if (game.checkWin(currentClass)) {
-            console.log (`${currentClass} wins`)
-        }
+            game.endGame(false);
+
         // Check For Draw
-        // Switch Turns
-        switchTurns();
+        } else if (game.isDraw()) {
+            game.endGame(true);
+        
+        // Switch turns
+        } else {
+            switchTurns();
+            setBoardHoverClass();
+            turns++;
+            console.log(turns);
+        }
+ 
+
+    }
+
+    // Initiate game
+    const startGame = () => {
+        circleTurn = false;
+        cellElements.forEach(cell => {
+            cell.classList.remove(X_CLASS);
+            cell.classList.remove(CIRCLE_CLASS);
+            cell.removeEventListener('click', handleClick, {once: true})
+        });
+        
+        // Event Listener to handle clicks
+        cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, {once: true})
+    });
         setBoardHoverClass();
+        
+        turns = 0;
     }
 
     // Switch from X to O or vice versa
@@ -79,6 +143,9 @@ const board = (() => {
 
     return {
         cellElements,
-
+        circleTurn,
+        startGame,
     }
 })();
+
+board.startGame();
